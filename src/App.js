42c596204c5a4333ce8d140233ad315d1ee5a351
홍래영 <hong5665@naver.com>
2022-08-23 18:04:45 +0900

    codacy test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [test, setTest] = useState(false);
+
+  useEffect(() => {
+    if (test) {
+      console.log("test > ", test);
+    }
+  }, []);
+
+  console.log("codacy test");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
