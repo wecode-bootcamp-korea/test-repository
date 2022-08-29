88d6a533fc39e9a5563a6757f411f74e3f907594
youjin <your.jiny.9@gmail.com>
2022-08-29 17:24:48 +0900

    오류 수정

--- src/Youjin.js
+++ src/Youjin.js
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Youjin() {
   const [state, setState] = useState();
   const [code, setCode] = useState(1);
-  console.log('test2');
-  console.log('test3');
-  console.log('test5');
-  console.log('test7');
-  console.log('test5');
-  console.log('test7');
-  console.log('test5');
-  console.log('test7');₩
+  console.log("test2");
+  console.log("test3");
+  console.log("test5");
+  console.log("test7");
+  console.log("test5");
+  console.log("test7");
+  console.log("test5");
+  console.log("test7");
 
-  const test = '1';
+  const test = "1";
 
   useEffect(() => {
     fetch(`http://www.wecode.kr/${code}`)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(setState);
   }, []);
 
   return (
     <div className="Youjin">
       <header className="App-header">
-        <img className="App-logo"  />
-        <p onClick={() => setCode(prev => prev + 1)}>Edit </p>
+        <img className="App-logo" />
+        <p onClick={() => setCode((prev) => prev + 1)}>Edit </p>
         <a
           className="App-link"
           href="https://reactjs.org"
